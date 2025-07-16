fa9961b9508efea0c3a989fa5f6c6dfa5acb8120
Aman Kumar <78886335+Aman-kumar-1999@users.noreply.github.com>
2025-07-16 20:13:40 +0530

    Add AiGeminiService Spring Boot microservice
    Introduced a new Spring Boot microservice 'aigeminiservice' with MongoDB reactive support and Eureka client integration. Includes initial Recommendation model, application configuration, Maven wrapper, and basic test setup. Updated project files for module inclusion.

---
+++ aigeminiservice/src/main/java/com/fitness/aigeminiservice/AigeminiserviceApplication.java
@@ -0,0 +1,14 @@
+package com.fitness.aigeminiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigeminiserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AigeminiserviceApplication.class, args);
+		System.out.println("AiGemini is running ........");
+	}
+
+}
---
+++ aigeminiservice/src/test/java/com/fitness/aigeminiservice/AigeminiserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.aigeminiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigeminiserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
